eb6f0a9a12875310d1d98b95bbbe0df56cf46c73
Martin Hohenberg <me@martinhohenberg>
2020-07-25 09:50:40 +0200

    Preparation for using config files

--- src/icaltw.py
+++ src/icaltw.py
@@ -1,9 +1,13 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 import http.server
 import socketserver
+import os
+from config_handler import ConfigHandler
 
-PORT = 8080
+config = ConfigHandler('./twconfig').read()
+
+PORT = int(config['SERVER']['port'])
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
